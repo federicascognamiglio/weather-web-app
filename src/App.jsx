81e5879a9765c90c673440659ce6dc1356a81840
federicascognamiglio <scognamigliofede99@gmail.com>
2025-06-15 16:32:08 +0200

    Initialize React weather web app with basic routing

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
